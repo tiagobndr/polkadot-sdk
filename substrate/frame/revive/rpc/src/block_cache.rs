20327a99bb97218c7569f55b1931a6943e521e74
pgherveou <pgherveou@gmail.com>
2024-12-08 15:19:32 +0100

    Make BlockCache generic over the Block so we can unit test it

--- substrate/frame/revive/rpc/src/block_cache.rs
+++ substrate/frame/revive/rpc/src/block_cache.rs
@@ -30,18 +30,33 @@ use tokio::sync::RwLock;
 /// For each block in the cache, we also store the EVM transaction receipts.
 pub const CACHE_SIZE: usize = 256;
 
+/// Abstraction over `SubstrateBlock` use to test `BlockCache` with `MockBlock`
+pub trait Block {
+	fn hash(&self) -> H256;
+	fn number(&self) -> SubstrateBlockNumber;
+}
+
+impl Block for SubstrateBlock {
+	fn hash(&self) -> H256 {
+		SubstrateBlock::hash(&self)
+	}
+	fn number(&self) -> u32 {
+		SubstrateBlock::number(&self)
+	}
+}
+
 /// The cache maintains a buffer of the last N blocks,
-#[derive(Default)]
-pub struct BlockCache<const N: usize = CACHE_SIZE> {
+#[derive(frame_support::DefaultNoBound)]
+pub struct BlockCache<const N: usize, Block> {
 	/// A double-ended queue of the last N blocks.
 	/// The most recent block is at the back of the queue, and the oldest block is at the front.
-	buffer: VecDeque<Arc<SubstrateBlock>>,
+	buffer: VecDeque<Arc<Block>>,
 
 	/// A map of blocks by block number.
-	blocks_by_number: HashMap<SubstrateBlockNumber, Arc<SubstrateBlock>>,
+	blocks_by_number: HashMap<SubstrateBlockNumber, Arc<Block>>,
 
 	/// A map of blocks by block hash.
-	blocks_by_hash: HashMap<H256, Arc<SubstrateBlock>>,
+	blocks_by_hash: HashMap<H256, Arc<Block>>,
 
 	/// A map of receipts by hash.
 	receipts_by_hash: HashMap<H256, ReceiptInfo>,
@@ -53,32 +68,22 @@ pub struct BlockCache<const N: usize = CACHE_SIZE> {
 	tx_hashes_by_block_and_index: HashMap<H256, HashMap<U256, H256>>,
 }
 
-#[derive(Clone)]
-pub struct BlockCacheProvider {
-	cache: Arc<RwLock<BlockCache<CACHE_SIZE>>>,
+#[derive(frame_support::CloneNoBound, frame_support::DefaultNoBound)]
+pub struct BlockCacheProvider<Block = SubstrateBlock> {
+	cache: Arc<RwLock<BlockCache<CACHE_SIZE, Block>>>,
 }
 
-impl Default for BlockCacheProvider {
-	fn default() -> Self {
-		Self { cache: Arc::new(RwLock::new(BlockCache::default())) }
-	}
-}
-
-impl BlockCacheProvider {
-	async fn cache(&self) -> tokio::sync::RwLockReadGuard<'_, BlockCache<CACHE_SIZE>> {
+impl<B: Block> BlockCacheProvider<B> {
+	async fn cache(&self) -> tokio::sync::RwLockReadGuard<'_, BlockCache<CACHE_SIZE, B>> {
 		self.cache.read().await
 	}
 
-	pub async fn ingest(
-		&self,
-		block: SubstrateBlock,
-		receipts: Vec<(TransactionSigned, ReceiptInfo)>,
-	) {
+	pub async fn ingest(&self, block: B, receipts: Vec<(TransactionSigned, ReceiptInfo)>) {
 		let mut cache = self.cache.write().await;
 		cache.insert(block, receipts);
 	}
 
-	pub async fn latest_block(&self) -> Option<Arc<SubstrateBlock>> {
+	pub async fn latest_block(&self) -> Option<Arc<B>> {
 		let cache = self.cache().await;
 		cache.buffer.back().cloned()
 	}
@@ -100,15 +105,12 @@ impl BlockCacheProvider {
 		cache.tx_hashes_by_block_and_index.get(block_hash).map(|v| v.len())
 	}
 
-	pub async fn block_by_number(
-		&self,
-		number: SubstrateBlockNumber,
-	) -> Option<Arc<SubstrateBlock>> {
+	pub async fn block_by_number(&self, number: SubstrateBlockNumber) -> Option<Arc<B>> {
 		let cache = self.cache().await;
 		cache.blocks_by_number.get(&number).cloned()
 	}
 
-	pub async fn block_by_hash(&self, hash: &H256) -> Option<Arc<SubstrateBlock>> {
+	pub async fn block_by_hash(&self, hash: &H256) -> Option<Arc<B>> {
 		let cache = self.cache().await;
 		cache.blocks_by_hash.get(hash).cloned()
 	}
@@ -124,13 +126,9 @@ impl BlockCacheProvider {
 	}
 }
 
-impl<const N: usize> BlockCache<N> {
+impl<const N: usize, B: Block> BlockCache<N, B> {
 	/// Insert an entry into the cache, and prune the oldest entry if the cache is full.
-	pub fn insert(
-		&mut self,
-		block: SubstrateBlock,
-		receipts: Vec<(TransactionSigned, ReceiptInfo)>,
-	) {
+	pub fn insert(&mut self, block: B, receipts: Vec<(TransactionSigned, ReceiptInfo)>) {
 		if self.buffer.len() >= N {
 			if let Some(block) = self.buffer.pop_front() {
 				log::trace!(target: LOG_TARGET, "Pruning block: {}", block.number());
@@ -168,5 +166,51 @@ impl<const N: usize> BlockCache<N> {
 		self.buffer.push_back(block.clone());
 		self.blocks_by_number.insert(block.number(), block.clone());
 		self.blocks_by_hash.insert(block.hash(), block);
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	struct MockBlock {
+		block_number: SubstrateBlockNumber,
+		block_hash: H256,
+	}
+
+	impl Block for MockBlock {
+		fn hash(&self) -> H256 {
+			self.block_hash
+		}
+		fn number(&self) -> u32 {
+			self.block_number
+		}
+	}
+
+	#[test]
+	fn cache_insert_and_prune_works() {
+		let mut cache = BlockCache::<2, MockBlock>::default();
+
+		let mut insert = |block_number: u8| {
+			let block_hash = H256::from([block_number; 32]);
+			cache.insert(
+				MockBlock { block_number: block_number.into(), block_hash },
+				vec![(
+					TransactionSigned::default(),
+					ReceiptInfo { transaction_hash: block_hash, ..Default::default() },
+				)],
+			);
+		};
+
+		insert(1);
+		insert(2);
+		insert(3);
+
+		assert_eq!(cache.buffer.len(), 2);
+		assert_eq!(cache.blocks_by_number.len(), 2);
+		assert_eq!(cache.blocks_by_hash.len(), 2);
+		assert_eq!(cache.receipts_by_hash.len(), 2);
+		assert_eq!(cache.signed_tx_by_hash.len(), 2);
+		assert_eq!(cache.tx_hashes_by_block_and_index.len(), 2);
 	}
 }
